ee0ac5f864c7f1386914b0fa3b9e9c704a2ef72d
Porteleki Gábor <gabesz880122@gmail.com>
2020-02-23 22:51:24 +0100

    Adding MonsterController for Api

---
+++ src/Mithrill.MonsterBook.WebApi/Controllers/MonstersController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mithrill.MonsterBook.Application.Monsters.Query.GetGeneratedMonster;
+using Mithrill.MonsterBook.Application.Monsters.Query.GetMonster;
+using Mithrill.MonsterBook.Application.Monsters.Query.GetMonsters;
+using Monster = Mithrill.MonsterBook.Application.Monsters.Query.GetMonsters.Monster;
+
+namespace Mithrill.MonsterBook.WebApi.Controllers
+{
+    public class MonstersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MonstersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Monster>), StatusCodes.Status200OK)]
+        public async Task<IEnumerable<Monster>> GetMonsters(CancellationToken cancellationToken)
+        {
+            var monsters = await _mediator.Send(new GetMonstersQuery(), cancellationToken);
+
+            return monsters;
+        }
+
+        [HttpGet("Id:int")]
+        public async Task<Application.Monsters.Query.GetMonster.Monster> GetMonster([FromQuery]GetMonsterQuery query, CancellationToken cancellationToken)
+        {
+            var monster = await _mediator.Send(query, cancellationToken);
+
+            return monster;
+        }
+
+        [HttpGet("{Id:int}/Generate")]
+        public async Task<GeneratedMonster> GetGeneratedMonster([FromQuery]GetGeneratedMonsterQuery query, CancellationToken cancellationToken)
+        {
+            var monster = await _mediator.Send(query, cancellationToken);
+
+            return monster;
+        }
+    }
+}
