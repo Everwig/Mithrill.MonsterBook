90c2950279550c5f1234aaeb1dea4e890d683704
Porteleki Gábor <gabesz880122@gmail.com>
2020-02-23 22:51:02 +0100

    Registering MediatR and AutoMapper

---
+++ src/Mithrill.MonsterBook.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Mithrill.MonsterBook.Application.Common.Adapters;
+using Mithrill.MonsterBook.Application.Common.Factories;
+using Mithrill.MonsterBook.Application.Common.Mapping;
+using Mithrill.MonsterBook.Application.Monsters.Query.GetGeneratedMonster;
+
+namespace Mithrill.MonsterBook.Application
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterApplication(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IMonsterFactory, MonsterFactory>();
+            serviceCollection.AddMediatR(typeof(GetGeneratedMonsterQuery).Assembly);
+            serviceCollection.AddAutoMapper(typeof(MappingProfile).Assembly);
+        }
+    }
+}
--- src/Mithrill.MonsterBook.WebApi/Startup.cs
+++ src/Mithrill.MonsterBook.WebApi/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Mithrill.MonsterBook.Application;
 
 namespace Mithrill.MonsterBook.WebApi
 {
@@ -19,6 +20,7 @@ namespace Mithrill.MonsterBook.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.RegisterApplication();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
